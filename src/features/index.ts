08ead2113eed6dced52499bc238d4943d46e15ec
m-Kai.Ouyang <843766158@qq.com>
2026-03-23 11:33:12 +0800

    feat(tableOfContents): 重构目录索引功能，支持子文档索引和大纲生成

--- src/features/index.ts
+++ src/features/index.ts
@@ -2,7 +2,7 @@
  * 功能模块统一导出
  */
 export { registerPageLock } from './pageLock'
-export { registerTableOfContents } from './tableOfContents'
+export { registerTableOfContents } from './tableOfContents/types'
 export { registerImageCompressor } from './imageCompressor'
 export { registerDocNavigation } from './docNavigation'
 export { registerShortcut, addCustomShortcut, addCustomShortcuts, getShortcutManager, type ShortcutInfo } from './shortcut'

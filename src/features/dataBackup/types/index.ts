a00f7730b8bc566ecb82594089ad5e90dffb4c87
m-Kai.Ouyang <843766158@qq.com>
2026-06-07 22:43:22 +0800

    feat(dataBackup): add auto backup plugin settings option
    add a new setting to control whether backup plugin data during auto backup, include:
    1. add autoBackupPluginData field to BackupSettings type
    2. add UI selector for the setting in backup page
    3. load and save the setting with other backup configs
    4. trigger plugin settings export when auto backup runs if enabled

--- src/features/dataBackup/types/index.ts
+++ src/features/dataBackup/types/index.ts
@@ -7,6 +7,7 @@ import { TypedStorage } from "@/utils/typedStorage"
 
 interface BackupSettings {
   autoBackupEnabled: boolean
+  autoBackupPluginData?: boolean
   backupFrequency: string
   backupTime: string
   keepBackupCount: number

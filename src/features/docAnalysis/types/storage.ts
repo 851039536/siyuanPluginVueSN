5398bb8205542171f91b48c01014dcf8baac2a58
cnb <843766158@qq.com>
2026-07-20 17:28:14 +0800

    feat(docAnalysis): 添加重名文档名称排除过滤功能

--- src/features/docAnalysis/types/storage.ts
+++ src/features/docAnalysis/types/storage.ts
@@ -13,10 +13,12 @@ import { TypedStorage } from "@/utils/typedStorage"
 export class DocAnalysisStorage {
   readonly options: TypedStorage<FilterOptions>
   readonly platformMeta: TypedStorage<PlatformMeta[]>
+  readonly duplicateNameFilter: TypedStorage<string[]>
 
   constructor(plugin: Plugin) {
     const storage = new PluginStorage(plugin)
     this.options = new TypedStorage(storage, "doc-analysis-options", DEFAULT_FILTER_OPTIONS)
     this.platformMeta = new TypedStorage(storage, "doc-analysis-platforms", DEFAULT_PLATFORM_META)
+    this.duplicateNameFilter = new TypedStorage(storage, "doc-analysis-dup-filter", [])
   }
 }

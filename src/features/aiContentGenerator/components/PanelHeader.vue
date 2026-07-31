ffebcb057739a8f16efb24c703320b340c5176c5
cnb <843766158@qq.com>
2026-07-31 17:15:08 +0800

    refactor(aiContentGenerator): 移除顶部工具栏及相关样式
    - 删除PanelHeader组件及其引用
    - 移除aiContentGenerator模块中顶部工具栏的模板代码
    - 删除PanelHeader.scss样式文件及其内容

--- src/features/aiContentGenerator/components/PanelHeader.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="panel-header">
-    <span class="panel-title">AI 内容生成</span>
-  </div>
-</template>
-
-<style lang="scss" scoped>
-@use "../styles/PanelHeader.scss";
-</style>

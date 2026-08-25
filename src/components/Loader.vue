54d699721215b79693b18afd05f55bc58c13ea64
kai <843766158@qq.com>
2026-08-25 16:49:08 +0800

    refactor(loader): 封装定位容器并优化动画与无障碍

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,13 +1,28 @@
+<!-- 纯 CSS 动画加载指示器（飞船 + 长光带），自带定位容器，供各功能加载态复用 -->
 <template>
-  <div class="loader">
-    <span><span></span><span></span><span></span><span></span></span>
-    <div class="base">
-      <span></span>
-      <div class="face"></div>
+  <div
+    class="loader-root"
+    role="status"
+    aria-label="加载中"
+  >
+    <div class="loader">
+      <span class="thruster">
+        <span class="thruster-ray"></span>
+        <span class="thruster-ray"></span>
+        <span class="thruster-ray"></span>
+        <span class="thruster-ray"></span>
+      </span>
+      <div class="base">
+        <span></span>
+        <div class="face"></div>
+      </div>
     </div>
-  </div>
-  <div class="longfazers">
-    <span></span><span></span><span></span><span></span>
+    <div
+      class="longfazers"
+      aria-hidden="true"
+    >
+      <span></span><span></span><span></span><span></span>
+    </div>
   </div>
 </template>
 
